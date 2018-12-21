ee6a9dc579bdb04a281846c03d58a40756da0a30
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2018-12-21 13:35:20 -0500

    A little more setup

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,41 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS potLucky;
+CREATE DATABASE potLucky;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE potLucky;
+
+CREATE TABLE kingTable
+(
+    id INT AUTO_INCREMENT NOT NULL,
+  host_name VARCHAR(255) NOT NULL,
+  host_id INT NOT NULL,
+  event_name VARCHAR(30) NOT NULL,
+  event_location VARCHAR(255) NOT NULL,
+  event_date TIMESTAMP NOT NULL,
+  event_description VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  PRIMARY KEY
+  (id)
+)
+
+CREATE TABLE eventTable
+(
+id INT AUTO_INCREMENT NOT NULL,
+item VARCHAR(100) NOT NULL,
+quantity INT NOT NULL,
+item_type VARCHAR(30) NOT NULL,
+cost DECIMAL,
+bringer_id INT,
+bringer_name VARCHAR(255),
+PRIMARY KEY
+(id)
+)
+
+CREATE TABLE users
+(
+    id INT AUTO_INCREMENT NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    PRIMARY KEY 
+    (id)
+)
+
