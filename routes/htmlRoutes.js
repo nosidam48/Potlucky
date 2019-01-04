cb1853e36bc1f5255153b1f47256c6b3e8bd2c17
Danielle Wood <danielle.wood9988@gmail.com>
2019-01-04 16:12:01 -0500

    log in or sign up on the frontpage is now clickable

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,58 +12,6 @@ module.exports = function (app) {
   });
 
 
-<<<<<<< HEAD
-  app.get("/host", function (req, res) {
-    res.render("host", { user: req.user });
-  });
-  app.get("/host/:id", function (req, res) {
-    res.render("host2", { event_id: req.params.id });
-  });
-  app.get("/view/:category?/:search?", function (req, res) {
-    if (req.params.category) {
-      var category = req.params.category;
-    }
-    else {
-      var category = "";
-    }
-    if (req.params.search) {
-      var search = req.params.search;
-    }
-    else {
-      var search = "event_date";
-    }
-    db.eventTable.findAll({
-      limit: 10,
-      where: {
-        "category": category,
-        event_date: {
-          [Op.gte]: Date.now()
-        }
-      },
-      order: [search]
-
-    }).then(function (dbEvents) {
-      res.render("view", {
-        events: dbEvents
-      });
-    });
-  });
-  app.get("/login", function (req, res) {
-    if (req.user) {
-      res.redirect("/");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-  app.get("/signup", function (req, res) {
-    if (req.user) {
-      res.redirect("/")
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
-=======
 app.get("/host", function (req, res) {
   res.render("host", {user: req.user});
 });
@@ -113,5 +61,4 @@ app.get("/signup", function(req, res) {
 app.get("*", function (req, res) {
   res.render("404");
 });
->>>>>>> 54e92fb2bab26300dd497678569b21cd14b6acc5
 };
