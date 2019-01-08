698fcd07b7ca035317d7de6c4f84b1047956699b
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2019-01-08 10:13:20 -0500

    please work

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(express.static("public"));
 
 // Handlebars
 app.engine(
